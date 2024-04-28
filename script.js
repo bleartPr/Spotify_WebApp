01b1f6afece3e6a94a2cf96c21f3cc06cf51aa11
Bleart Prekadini <148566456+bleartPr@users.noreply.github.com>
2024-04-28 14:36:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+function display_content() {
+    fetch('http://localhost:3000/test_api_token')
+        .then(() => {
+            const user_search = document.querySelector(".input-field").value;
+
+            fetch(`http://localhost:3000/get_artist_search`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ search: user_search })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    let artist_id;
+                    try {
+                        artist_id = data.artists.items[0].id;
+                    } catch (error) {
+                        document.getElementById("error-message").innerHTML = "Sorry, This Artist Is Unavailable";
+                        return;
+                    }
+
+                    fetch('http://localhost:3000/get_artist_info')
+                        .then(response => response.json())
+                        .then(data => {
+                            if (document.getElementById("error-message").innerHTML !== "") {
+                                document.getElementById("error-message").innerHTML = "";
+                            }
+
+                            try {
+                                document.querySelector(".photo").style.backgroundImage = `url(${data.images[0].url})`;
+                                document.querySelector(".photo").style.backgroundSize = 'cover';
+                                document.querySelector(".photo").classList.remove("hide");
+                                document.querySelector(".photo-missing").classList.add("hide");
+                                document.getElementById("photo-clickable").href = `https://open.spotify.com/artist/${artist_id}`;
+                            } catch (error) {
+                                document.querySelector(".photo").classList.add("hide");
+                                document.querySelector(".photo-missing").classList.remove("hide");
+                            }
+
+                            const topGenres = data.genres.slice(0, 3);
+                            for (let i = 1; i <= 3; i++) {
+                                const genreRow = document.querySelector(`.genre-row-${i}`);
+                                if (topGenres[i - 1]) {
+                                    genreRow.innerHTML = topGenres[i - 1];
+                                    genreRow.classList.remove("hide");
+                                } else {
+                                    genreRow.innerHTML = "";
+                                    genreRow.classList.add("hide");
+                                }
+                            }
+                            document.querySelector(".genres").classList.remove("hide");
+
+                            document.querySelector("#followers-text").innerHTML = data.followers.total.toLocaleString();
+                            document.querySelector(".followers").classList.remove("hide");
+
+                            document.getElementById("name-text").innerHTML = data.name;
+
+                            fetch('http://localhost:3000/get_artist_top_tracks')
+                                .then(response => response.json())
+                                .then(data => {
+                                    const topTracks = data.tracks.sort((a, b) => b.popularity - a.popularity).slice(0, 5);
+                                    for (let i = 1; i <= 5; i++) {
+                                        const trackText = document.getElementById(`track${i}-text`);
+                                        const trackPhoto = document.getElementById(`track${i}-photo`);
+                                        const trackLink = document.getElementById(`tracks-clickable-${i}`);
+                                        const track = topTracks[i - 1];
+
+                                        if (track) {
+                                            trackText.innerHTML = track.name;
+                                            trackPhoto.src = track.album.images[0].url;
+                                            trackLink.href = `https://open.spotify.com/track/${track.id}`;
+                                            [trackText, trackPhoto, trackLink].forEach(el => el.classList.remove("hide"));
+                                        } else {
+                                            trackText.innerHTML = "";
+                                            trackPhoto.src = "";
+                                            [trackText, trackPhoto, trackLink].forEach(el => el.classList.add("hide"));
+                                        }
+                                    }
+                                    document.querySelector(".top-tracks").classList.remove("hide");
+                                });
+                                fetch('http://localhost:3000/get_artist_album')
+                                .then(response => response.json())
+                                .then(data => {
+                                    const topAlbums = data.items.sort((a, b) => new Date(b.release_date) - new Date(a.release_date)).slice(0, 5);
+
+                                    let isEmpty = true;
+                    
+                                    // Clear the previous artist's albums and display the top albums
+                                    for (let i = 1; i <= 5; i++) {
+                                        const albumText = document.getElementById(`album${i}-text`);
+                                        const albumPhoto = document.getElementById(`album${i}-photo`);
+                                        const albumLink = document.getElementById(`album-clickable-${i}`);
+                                        const album = topAlbums[i - 1];
+                    
+                                        if (album) {
+                                            albumText.innerHTML = album.name;
+                                            albumPhoto.src = album.images[0].url;
+                                            albumLink.href = `https://open.spotify.com/album/${album.id}`;
+                                            [albumText, albumPhoto, albumLink].forEach(el => el.classList.remove("hide"));
+                    
+                                            isEmpty = false;
+                                            document.querySelector(".missing-albums-error").classList.add("hide");
+                                        } else {
+                                            albumText.innerHTML = "";
+                                            albumPhoto.src = "";
+                                            [albumText, albumPhoto, albumLink].forEach(el => el.classList.add("hide"));
+                                        }
+                                    }
+                                    if (isEmpty) {
+                                        document.querySelector(".missing-albums-error").classList.remove("hide");
+                                    }
+                                    document.querySelector(".albums").classList.remove("hide");
+                                })
+                        });
+                });
+        });
+}
